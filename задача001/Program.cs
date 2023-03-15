9411107b11e81204d43bb369523eb39cd766a6e2
Marat Berniyazov <berniyazovmarat1997@gmail.com>
2023-03-15 22:30:01 +0500

    add homework

---
+++ задача001/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Найдем максимальное значение из двух чисел");
+Console.Write("Введите первое число: ");
+int number1 = int.Parse(Console.ReadLine())!;
+Console.Write("Введите второе число: ");
+int number2 = int.Parse(Console.ReadLine())!;
+
+if (number1 > number2)
+{
+    Console.WriteLine($"Первое число больше второго, {number1} > {number2}");
+}
+else
+{
+    Console.WriteLine($"Второе число больше первого, {number2} > {number1}");  
+}
+
+
